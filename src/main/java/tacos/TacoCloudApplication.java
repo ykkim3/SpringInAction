ae2c230c7e55f6e46a1dd7c02f2cdc293fcb73cf
ykkim3-user <yunkiforever@gmail.com>
2021-04-15 16:45:33 +0900

    first commit

---
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -0,0 +1,18 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudApplication.class, args);
+		
+		
+	}
+
+}
---
+++ src/test/java/tacos/TacoCloudApplicationTests.java
@@ -0,0 +1,14 @@
+package tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	
+
+}
