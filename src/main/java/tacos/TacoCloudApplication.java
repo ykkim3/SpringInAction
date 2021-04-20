d82b7814fee41e52e53c50e8091388234b3d4a38
ykkim3-user <yunkiforever@gmail.com>
2021-04-20 18:15:28 +0900

    add spring security first

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -1,11 +1,16 @@
 package tacos;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import tacos.Ingredient.Type;
+import tacos.data.IngredientRepository;
 
 @SpringBootApplication
 public class TacoCloudApplication implements WebMvcConfigurer{
@@ -15,9 +20,24 @@ public class TacoCloudApplication implements WebMvcConfigurer{
 		SpringApplication.run(TacoCloudApplication.class, args);
 	}
 
-	
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("home");
+	@Bean
+	public CommandLineRunner dataLoader(IngredientRepository repo) {
+		return new CommandLineRunner() {
+			
+			@Override
+			public void run(String... args) throws Exception {
+				repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+				repo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+				repo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+				repo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+				repo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+				repo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+				repo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
+				repo.save(new Ingredient("JACK", "Monterrey", Type.CHEESE));
+				repo.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
+				repo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+			}
+		};
 	}
+
 }
