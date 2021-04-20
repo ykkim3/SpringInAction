2c7040093f20aeac1fa5f3af34147d27ff34d266
ykkim3-user <yunkiforever@gmail.com>
2021-04-20 17:38:15 +0900

    commit

--- src/main/java/tacos/data/TacoRepository.java
+++ src/main/java/tacos/data/TacoRepository.java
@@ -1,8 +1,10 @@
 package tacos.data;
 
+import org.springframework.data.repository.CrudRepository;
+
 import tacos.Taco;
 
-public interface TacoRepository {
+public interface TacoRepository extends CrudRepository<Taco, Long>{
 
-	Taco save(Taco design);
+//	Taco save(Taco design);
 }
