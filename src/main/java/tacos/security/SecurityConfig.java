65c91ab82559fee61ccf60510a383da99fa56004
ykkim3-user <yunkiforever@gmail.com>
2021-04-21 18:12:40 +0900

    page150

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -30,7 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
-		http.authorizeRequests()
+		http
+		.authorizeRequests()
 		.antMatchers("/design", "/orders")
 		.access("hasRole('ROLE_USER')")
 		.antMatchers("/","/**").access("permitAll")
