b26f7fa1a190fc528cc96d8cf88a49adb893afe2
ykkim3-user <yunkiforever@gmail.com>
2021-04-21 15:16:21 +0900

    commit spring security part

---
+++ src/main/java/tacos/security/NoEncodingPasswordEndoder.java
@@ -0,0 +1,18 @@
+package tacos.security;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class NoEncodingPasswordEndoder implements PasswordEncoder{
+
+	@Override
+	public String encode(CharSequence rawPassword) {
+		return rawPassword.toString();
+	}
+
+	@Override
+	public boolean matches(CharSequence rawPassword, String encodedPassword) {
+		return rawPassword.toString().equals(encodedPassword);
+	}
+
+	
+}
--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,14 +1,32 @@
 package tacos.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	DataSource dataSource;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
@@ -20,18 +38,56 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		.httpBasic();
 	}
 
+	/*
+	 * inmemory store
+	 */
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.inMemoryAuthentication()
+//		.withUser("user1")
+//		.password("{noop}password1")
+//		.authorities("ROLE_USER")
+//		.and()
+//		.withUser("user2")
+//		.password("{noop}password2")
+//		.authorities("ROLE_USER");
+//	}
+
+	/*
+	 * jdbc store
+	 */
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.jdbcAuthentication().dataSource(dataSource)
+//		.usersByUsernameQuery("select username, password, enabled from users where username=?")
+//		.authoritiesByUsernameQuery("select username, authority from authorities where username=?")
+//		.passwordEncoder(new NoEncodingPasswordEndoder());
+//	}
+	
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth
+//		.ldapAuthentication()
+//		.userSearchBase("ou=people")
+//		.userSearchFilter("(uid={0})")
+//		.groupSearchBase("ou=groups")
+//		.groupSearchFilter("member={0}")
+//		.contextSource()
+////		.root("dc=tacocloud,dc=com")
+////		.ldif("classpath:users.ldif")
+//		.url("ldap://localhost:8389/dc=tacocloud,dc=com")
+//		.and()
+//		.passwordCompare()
+////		.passwordEncoder(new BCryptPasswordEncoder())
+//		.passwordEncoder(new NoEncodingPasswordEndoder())
+//		.passwordAttribute("userPasscode");
+//	}
+//	
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		.withUser("user1")
-		.password("{noop}password1")
-		.authorities("ROLE_USER")
-		.and()
-		.withUser("user2")
-		.password("{noop}password2")
-		.authorities("ROLE_USER");
+		auth
+			.userDetailsService(userDetailsService)
+			.passwordEncoder(encoder());
 	}
-
-
-	
 }
